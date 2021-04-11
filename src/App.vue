64f0f1745e83328d051a1885c11daf7869e4cf25
David Manso <davidmf@empathy.co>
2021-04-11 12:05:52 +0200

    Init vue project

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import HelloWorld from './components/HelloWorld.vue';
+
+@Component
+export default class App extends Vue {}
+</script>
+
+<style>
+</style>
